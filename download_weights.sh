d16d5039d89536f17ad40c58aa67a039b47f837c
Krithic Kumar <krithickumarub@protonmail.com>
2023-11-23 01:19:48 +0530

    add download scripts for weights

---
+++ download_weights.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -O 'svd.safetensors' 'https://huggingface.co/stabilityai/stable-video-diffusion-img2vid/resolve/main/svd.safetensors?download=true'
+wget -O 'svd_image_decoder.safetensors' 'https://huggingface.co/stabilityai/stable-video-diffusion-img2vid/resolve/main/svd_image_decoder.safetensors?download=true'
---
+++ download_xt_weights.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -O 'svd_xt.safetensors' 'https://huggingface.co/stabilityai/stable-video-diffusion-img2vid-xt/resolve/main/svd_xt.safetensors?download=true'
+wget -O 'svd_xt_image_decoder.safetensors' 'https://huggingface.co/stabilityai/stable-video-diffusion-img2vid-xt/resolve/main/svd_xt_image_decoder.safetensors?download=true'
