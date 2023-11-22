1ec1af8e803efabdcbcc5ae96236ef7f6569e5ef
Krithic Kumar <krithickumarub@protonmail.com>
2023-11-23 01:16:54 +0530

    add install script

---
+++ install.py
@@ -0,0 +1,25 @@
+import launch
+from importlib_metadata import version
+
+MIN_VERSION = "0.23.1"
+
+
+def install():
+    needs_install = False
+    if launch.is_installed("diffusers"):
+        installed_version = tuple(version("diffusers").split("."))
+        min_version = tuple(MIN_VERSION.split("."))
+        if installed_version < min_version:
+            needs_install = True
+    else:
+        needs_install = True
+
+    if needs_install:
+        try:
+            launch.run_pip(f"install diffusers>={MIN_VERSION}", "requirements for SVD")
+        except Exception as e:
+            print(e)
+            print("Can't install diffusers.")
+
+
+install()
