60a119c3d1089a06ccb02e3ae9b118d17f6e808c
Krithic Kumar <krithickumarub@protonmail.com>
2023-11-23 03:03:16 +0530

    remove download scripts

--- download_weights.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget -O 'svd.safetensors' 'https://huggingface.co/stabilityai/stable-video-diffusion-img2vid/resolve/main/svd.safetensors?download=true'
-wget -O 'svd_image_decoder.safetensors' 'https://huggingface.co/stabilityai/stable-video-diffusion-img2vid/resolve/main/svd_image_decoder.safetensors?download=true'
